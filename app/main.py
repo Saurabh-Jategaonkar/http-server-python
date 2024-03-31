f04e17383fd08fb2939bf902a62b843660bbaf02
Saurabh Jategaonkar <sj749@njit.edu>
2024-03-31 03:59:22 -0400

    post a file

--- app/main.py
+++ app/main.py
@@ -1,120 +1,80 @@
-import os
-import sys
-import threading
-import socket
-from typing import Optional
-import argparse
+import sys, os, socket, threading, re
 
-HOST = "localhost"
-PORT = 4221
-VERSION = b"HTTP/1.1"
-OK_MESSAGE = b"OK"
-OK_STATUS_CODE = b"200"
-NOT_FOUND_MESSAGE = b"Not Found"
-1
-NOT_FOUND_STATUS_CODE = b"404"
-def process_request(conn: socket.socket, addr: str, args: argparse.Namespace) -> None:
-    data = conn.recv(1024)
-    data_str = data.decode("utf-8")
-    lines = data_str.split("\r\n")
-    assert len(lines) > 0
-    start_line = lines[0]
-    parts = start_line.split(" ")
-    assert len(parts) == 3
-    method = parts[0]
-    path = parts[1]
-    version = parts[2]
-    assert method == "GET"
-    assert version == "HTTP/1.1"
-    path_components = [c for c in path.split("/") if c]
-    request_headers = []
-    for line in lines[1:]:
-        if not line:
-            break
-        header_parts = line.split(": ")
-        assert len(header_parts) == 2
-        header_type = header_parts[0]
-        header_content = header_parts[1]
-        request_headers.append((header_type, header_content))
-    response_headers = []
-    response_body: Optional[str] = None
-    if len(path_components) == 0:
-        req_status_code = OK_STATUS_CODE
-        req_message = OK_MESSAGE
-    elif path_components[0] == "echo":
-        req_status_code = OK_STATUS_CODE
-        req_message = OK_MESSAGE
-        path_suffix = "/".join(path_components[1:])
-        response_headers = [
-            "Content-Type: text/plain",
-            f"Content-Length: {len(path_suffix)}",
-        ]
-        response_body = path_suffix
-    elif path_components[0] == "user-agent":
-        req_status_code = OK_STATUS_CODE
-        req_message = OK_MESSAGE
-        user_agent = None
-        for header in request_headers:
-            if header[0] == "User-Agent":
-                user_agent = header[1]
-                break
-        if user_agent:
-            response_headers = [
-                "Content-Type: text/plain",
-                f"Content-Length: {len(user_agent)}",
-            ]
-            response_body = user_agent
+def http_response(conn, addr, directory=None):
+    data: bytes = conn.recv(1024).decode()
+    lines = data.split("\r\n")
+    method = lines[0].split(" ")[0]
+    path = lines[0].split(" ")[1]
+    echo_keyword = path.split("/")[1]
+
+    body = lines[-1]
+    if path == "/":
+        conn.send(b"HTTP/1.1 200 OK\r\n\r\n")
+    elif echo_keyword == "echo":
+        pattern = r"/echo/(.*)"
+        match = re.search(pattern, path)
+        response_body = ""
+        if match:
+            response_body = match.group(1)
+        response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(response_body)}\r\n\r\n{response_body}"
+        conn.send(response.encode())
+    elif echo_keyword == "user-agent":
+        user_agent = lines[2].split(" ")[1]
+        response_body = user_agent
+        response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(response_body)}\r\n\r\n{response_body}"
+        conn.send(response.encode())
+    elif echo_keyword == "files":
+        pattern = r"/files/(.*)"
+        match = re.search(pattern, path)
+        filename = ""
+        if match:
+            filename = match.group(1)
+        data = "no file"
+        if method == "POST":
+            filepath = os.path.join(directory, filename)
+            file_object = open(filepath, "w")
+            file_object.write(body)
+            file_object.close()
+            data = "file created"
+            response = "HTTP/1.1 201 Created\r\n"
+            response += "Content-Type: text/plain\r\n"
+            response += f"Content-Length: {len(data)}\r\n\r\n"
+            response += data
+
+            conn.send(response.encode())
         else:
-            req_status_code = NOT_FOUND_STATUS_CODE
-            req_message = NOT_FOUND_MESSAGE
-    elif path_components[0] == "files":
-        assert args.directory is not None
-        files = [
-            f
-            for f in os.listdir(args.directory)
-            if os.path.isfile(os.path.join(args.directory, f))
-        ]
-        expected_file = path_components[1]
-        if expected_file in files:
-            req_status_code = OK_STATUS_CODE
-            req_message = OK_MESSAGE
-            with open(os.path.join(args.directory, expected_file), "r") as f:
-                response_body = f.read()
-            response_headers = [
-                "Content-Type: application/octet-stream",
-                f"Content-Length: {len(response_body)}",
-            ]
-        else:
-            req_status_code = NOT_FOUND_STATUS_CODE
-            req_message = NOT_FOUND_MESSAGE
+            if filename in os.listdir(directory):
+                filepath = os.path.join(directory, filename)
+                file_object = open(filepath)
+                data = file_object.read()
+                file_object.close()
+                response = "HTTP/1.1 200 OK\r\n"
+                response += "Content-Type: application/octet-stream\r\n"
+                response += f"Content-Length: {len(data)}\r\n\r\n"
+                response += data
+            else:
+                response = "HTTP/1.1 404 Not Found\r\n"
+                response += "Content-Type: text/plain\r\n"
+                response += f"Content-Length: {len(data)}\r\n\r\n"
+                response += data
+            conn.send(response.encode())
     else:
-        req_status_code = NOT_FOUND_STATUS_CODE
-        req_message = NOT_FOUND_MESSAGE
-    response = b"%s %s %s\r\n" % (VERSION, req_status_code, req_message)
-    for header in response_headers:
-        response += header.encode("utf-8") + b"\r\n"
-    if response_body:
-        response += b"\r\n" + response_body.encode("utf-8")
-    response += b"\r\n"
-    conn.send(response)
+        conn.send(b"HTTP/1.1 404 Not Found\r\n\r\n")
     conn.close()
 
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--directory", default=None, required=False, type=str)
-    args = parser.parse_args()
-    server_socket = socket.create_server((HOST, PORT), reuse_port=True)
-    running = True
-    thread_pool = []
-    while running:
-        conn, addr = server_socket.accept()
-        t = threading.Thread(target=process_request, args=(conn, addr, args))
-        thread_pool.append(t)
-        t.start()
-    print("Shutting down server...")
-    server_socket.close()
-    for t in thread_pool:
-        t.join()
+def main(directory=None):
+    print("Logs from your program will appear here!")
+    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
+    while True:
+        client_socket, client_address = server_socket.accept()
+        print(f"Received a connection from {client_address}")
+        threading.Thread(
+            target=http_response, args=[client_socket, client_address, directory]
+        ).start()
+
 if __name__ == "__main__":
-    main()
-    sys.exit(main())
+    if len(sys.argv) == 3 and sys.argv[1] == "--directory":
+        directory = sys.argv[2]
+        main(directory)
+    else:
+        main()
