3ba029c31042d2494e091fcc3af879879f5b5ef4
Saurabh Jategaonkar <sj749@njit.edu>
2024-03-31 03:45:51 -0400

    parsing headers

--- app/main.py
+++ app/main.py
@@ -1,12 +1,14 @@
 import socket
-OK_200 = b"HTTP/1.1 200 OK\r\n\r\n"
-ERR_404 = b"HTTP/1.1 404 Not Found\r\n\r\n"
+
+OK_200 = "HTTP/1.1 200 OK\r\n"
+ERR_404 = "HTTP/1.1 404 Not Found\r\n"
 
 def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     conn, _ = server_socket.accept()  # wait for client
     data = conn.recv(1024)
     response = parse_msg(data)
+    print(f"response: {response}")
     conn.send(response)
 
 def parse_msg(msg: bytes):
@@ -14,18 +16,29 @@ def parse_msg(msg: bytes):
     top_header = headers[0]
     top_header_split = top_header.split(b" ")
     path = top_header_split[1]
-    content = path.decode("utf-8")
+    print(path)
+    print(content)
+
     if path == b"/":
-        return OK_200
-    if not content.startswith("/echo/"):
-        return ERR_404
-    content = content.removeprefix("/echo/")
-    if content:
+        return f"{OK_200}\r\n".encode()
+    elif content.startswith("/echo/"):
+        content = content.removeprefix("/echo/")
         resp = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(content)}\r\n\r\n{content}\r\n\r\n".encode()
         print(f"resp: {resp}")
         return resp
+    elif content.startswith("/user-agent"):
+        agent_header = [
+            h.decode("utf-8")
+            for h in headers
+            if h.decode("utf-8").startswith("User-Agent")
+        ][0]
+        agent = agent_header.replace("User-Agent: ", "")
+        return response_template(status=OK_200, type="text/plain", content=agent)
     else:
-        return ERR_404
+        return f"{ERR_404}\r\n".encode()
     
+def response_template(status: str, type: str, content: str):
+    return f"{status}Content-Type: {type}\r\nContent-Length: {len(content)}\r\n\r\n{content}\r\n\r\n".encode()
+
 if __name__ == "__main__":
     main()
