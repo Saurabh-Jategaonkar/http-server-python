b479e4df7e69d536ef6f2a0c8aa9e823648618da
Saurabh Jategaonkar <sj749@njit.edu>
2024-03-31 03:42:01 -0400

    responding with content

--- app/main.py
+++ app/main.py
@@ -1,20 +1,31 @@
-# Uncomment this to pass the first stage
 import socket
+OK_200 = b"HTTP/1.1 200 OK\r\n\r\n"
+ERR_404 = b"HTTP/1.1 404 Not Found\r\n\r\n"
+
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
-    # Uncomment this to pass the first stage
-    #
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    client_socket, address = server_socket.accept()  # wait for client
-    data = client_socket.recv(1024)
-    response = "HTTP/1.1 200\r\n\r\n"
-    lines = data.decode().split("\r\n")
-    verb, path, version = lines[0].split(" ")
-    if path == "/":
-        response = "HTTP/1.1 200\r\n\r\n"
+    conn, _ = server_socket.accept()  # wait for client
+    data = conn.recv(1024)
+    response = parse_msg(data)
+    conn.send(response)
+
+def parse_msg(msg: bytes):
+    headers = msg.split(b"\r\n")
+    top_header = headers[0]
+    top_header_split = top_header.split(b" ")
+    path = top_header_split[1]
+    content = path.decode("utf-8")
+    if path == b"/":
+        return OK_200
+    if not content.startswith("/echo/"):
+        return ERR_404
+    content = content.removeprefix("/echo/")
+    if content:
+        resp = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(content)}\r\n\r\n{content}\r\n\r\n".encode()
+        print(f"resp: {resp}")
+        return resp
     else:
-        response = "HTTP/1.1 404\r\n\r\n"
-    client_socket.send(response.encode())
+        return ERR_404
+    
 if __name__ == "__main__":
     main()
